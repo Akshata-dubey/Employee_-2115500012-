2804de70fe663078dfdf0c3e5a4a1e5294ce55ae
Akshata Dubey <akshatadubey213@gmail.com>
2025-03-19 14:50:23 +0530

    Adding UC3 files

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,13 +1,15 @@
-const IS_ABSENT = 0
+// empCheck = Math.floor(Math.random() * 10) % 3;
 
-
-// let empCheck = Math.floor(Math.random()*10) % 2;
-// if(empCheck == IS_ABSENT)
-// {
-//     console.log("Employee is Absent");
-//     return ;
-
-// }else 
-// {
-//     console.log("Employee is PRESENT");
+// switch (empCheck) {
+//     case IS_PART_TIME:
+//         empHrs = PART_TIME_HOURS;
+//         break;
+//     case IS_FULL_TIME:
+//         empHrs = FULL_TIME_HOURS;
+//         break;
+//     default:
+//         empHrs = 0;
 // }
+
+// let empWage = empHrs * WAGE_PER_HOUR;
+// console.log("Emp Wage: " + empWage);
